467eee672a492105b324f327066c19ecdb3d7924
pinetreelch <chanho.lee0215@gmail.com>
2022-07-29 10:48:42 +0900

    슬라이더 구현 2일차

--- public/ridibooks/user/book/slideShow.js
+++ public/ridibooks/user/book/slideShow.js
@@ -1,34 +1,119 @@
-const slides = document.querySelector('.slides'); //전체 슬라이드 컨테이너
-const slideImg = document.querySelectorAll('.slides li'); //모든 슬라이드들
-let currentIdx = 0; //현재 슬라이드 index
-const slideCount = slideImg.length; // 슬라이드 개수
-const prev = document.querySelector('.prev'); //이전 버튼
-const next = document.querySelector('.next'); //다음 버튼
-const slideWidth = 366; //한개의 슬라이드 넓이
-const slideMargin = 5; //슬라이드간의 margin 값
+var Slider = function(id, _web, _tab, _mobile, spacing){
+  var containerWidth = 0;
+  var sliderItemWidth = 0;
+  var totalCount = 0;
+  var spacing = spacing || 10;
+  var display = _web;
+  var left = 0;
+  var interval;
 
-//전체 슬라이드 컨테이너 넓이 설정
-slides.style.width = (slideWidth + slideMargin) * 7 + 'px';
+  var DOM = {
+    container: function(id){
+      var dom = document.querySelector('#'+id);
+      dom.className = 's-container';
+      dom.style.position = 'relative';
+      dom.style.overflow = 'hidden';
+      return dom;
+    },
+    slider: function(container){
+      totalCount = container.children.length;
 
-function moveSlide(num) {
-  slides.style.left = -num * 400 + 'px';
-  currentIdx = num;
-}
-
-prev.addEventListener('click', function () {
-  /*첫 번째 슬라이드로 표시 됐을때는 
-  이전 버튼 눌러도 아무런 반응 없게 하기 위해 
-  currentIdx !==0일때만 moveSlide 함수 불러옴 */
-
-  if (currentIdx !== 0) moveSlide(currentIdx - 1);
-});
-
-next.addEventListener('click', function () {
-  /* 마지막 슬라이드로 표시 됐을때는 
-  다음 버튼 눌러도 아무런 반응 없게 하기 위해
-  currentIdx !==slideCount - 1 일때만 
-  moveSlide 함수 불러옴 */
-  if (currentIdx !== slideCount - 1) {
-    moveSlide(currentIdx + 1);
+      var dom = document.createElement('div');
+      dom.className = 'slider'
+      dom.style.position = 'relative';
+      dom.style.overflow = 'hidden';
+      dom.style.height = '100%';
+      dom.style.left = 0;
+      dom.style.transition = 'left .5s';
+      return dom;
+    }
   }
-});
+
+  // DOM 만들기
+  var container = DOM.container(id);
+  var slider = DOM.slider(container);
+  var temp = container.innerHTML;
+  container.innerHTML = '';
+  slider.innerHTML = temp;
+  container.appendChild(slider);
+  var items = document.querySelector('#'+ id + ' .slider').children;
+  for(var i=0; i<items.length; i++){
+    items[i].style.float = 'left';
+    items[i].style.height = '100%';
+    items[i].style.width = (sliderItemWidth-spacing)+ 'px';
+    items[i].style['margin-right'] = spacing+'px'; // 간격
+  }
+
+  // 화면 사이즈 수정시 발생하는 이벤트
+  function resize(){
+    left = 0;
+    document.querySelector('#'+ id + ' .slider').style.left = left + 'px';
+
+    var innerWidth = window.innerWidth;
+    if(innerWidth >= 1000){
+      setDisplayCount(_web);
+    }else if(innerWidth < 1000 && innerWidth >= 768) {
+      setDisplayCount(_web);
+    }else if (innerWidth < 768) {
+      setDisplayCount(_web);
+    }
+    
+    if(display === 1){
+      spacing = 0;
+      var items = document.querySelector('#'+ id + ' .slider').children;
+      for(var i=0; i<items.length; i++){
+        items[i].style.width = sliderItemWidth + 'px';
+        items[i].style['margin-right'] = 0 + 'px'; // 간격
+      }
+    }
+  }
+
+  // 디스플레이 갯수 설정 함수
+  function setDisplayCount(count) {
+    display = count;
+
+    containerWidth = container.offsetWidth + spacing;
+    sliderItemWidth = containerWidth / display;
+
+    document.querySelector('#'+ id + ' .slider').style.width = totalCount * sliderItemWidth + spacing * totalCount + 'px';
+    var items = document.querySelector('#'+ id + ' .slider').children;
+    for(var i=0; i<items.length; i++){
+      items[i].style.width = (sliderItemWidth-spacing)+ 'px';
+    }
+  }
+
+  // 반응형 디스플레이 갯수 조절
+  var isResponsive = _tab != undefined && _mobile != undefined;
+  if(isResponsive){
+    window.onresize = resize;
+  }
+  resize();
+
+
+  return {
+    setDisplayCount: setDisplayCount,
+    move: function(index){
+      left = (-1) * sliderItemWidth * index;
+      document.querySelector('#'+ id + ' .slider').style.left = left + 'px';
+    },
+    prev: function(){
+      left += sliderItemWidth;
+      var limit = 0;
+      if(left > limit){
+        left = limit;
+      }
+      document.querySelector('#'+ id + ' .slider').style.left = left + 'px';
+    },
+    next: function(){
+      left -= sliderItemWidth;
+      var limit = (-1) * sliderItemWidth * (totalCount - display);
+      if(left < limit){
+        left = limit;
+      }
+      document.querySelector('#'+ id + ' .slider').style.left = left + 'px';
+    },
+    
+  }
+}
+var slider = new Slider('slider', 4, 3, 1, 20);
+slider.auto();
